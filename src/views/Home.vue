b3b1bc8a441c4439529faf6a606400d7c57d1b60
volibear <zhaop2014@sina.com>
2020-09-24 18:41:36 +0800

    修改eslint配置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  methods: {
+    triggerFn() {
+      this.$emit('submit', 'hello');
+    }
+  },
+  mounted() {
+    this.$on(['submit', 'commit'], this.handleEvents); // 以数组方式监听事件
+
+    this.$on('submit', this.handleEvents); // 监听到同一个事件后可执行多个回调方法
+    this.$on('submit', this.handleEvents1); // 监听到同一个事件后可执行多个回调方法
+    this.$on('submit', this.handleEvents2); // 监听到同一个事件后可执行多个回调方法
   }
-}
+};
 </script>
