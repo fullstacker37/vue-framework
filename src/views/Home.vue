fcd18c19a33fe4653ede5c76c89907f4f740b9fb
volibear <zhaop2014@sina.com>
2020-10-06 12:32:58 +0800

    dynamic slot

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <slot name="header" :user="obj" :section="'header'">
+      <div>默认header</div>
+    </slot>
+    <slot :user="obj" :section="'body'">默认body</slot>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  },
-  methods: {
-    triggerFn() {
-      this.$emit('submit', 'hello');
+  name: "Home",
+  data () {
+    return {
+      obj: { a: 1, b: 2 }
     }
   },
+  methods: {
+    
+  },
   mounted() {
-    this.$on(['submit', 'commit'], this.handleEvents); // 以数组方式监听事件
-
-    this.$on('submit', this.handleEvents); // 监听到同一个事件后可执行多个回调方法
-    this.$on('submit', this.handleEvents1); // 监听到同一个事件后可执行多个回调方法
-    this.$on('submit', this.handleEvents2); // 监听到同一个事件后可执行多个回调方法
-  }
+    
+  },
 };
 </script>
